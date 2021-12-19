ef8ea8a71da692118a7c5493a0f89a9959863c25
BK <benkoshy@hotmail.com>
2021-12-20 10:27:37 +1100

    WIP: monty_hall problem

--- lib/monty_hall.rb
+++ lib/monty_hall.rb
@@ -4,5 +4,41 @@ require_relative "monty_hall/version"
 
 module MontyHall
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Game
+
+    # doors numbered from 1 to x
+    attr_reader :doors
+
+    def initialize(doors: 3)
+      @doors = doors
+
+      @winning_door = pick_doors
+      @my_choice = pick_doors
+    end
+
+    def always_switch
+      if lost?
+        # open_doors
+        # choose from doors that are: (i) not the winning door, nor the door that was initially chosen
+
+        # pick again
+        # set result
+      else
+
+      end
+    end
+
+    def open_doors
+      1..@doors - [@my_choice, @winning_door]
+    end
+
+    def lost?
+      @winning_door != @my_choice
+    end
+
+    def pick_doors()
+      rand(1..@doors)
+    end
+  end
 end
--- test/monty_hall_test.rb
+++ test/monty_hall_test.rb
@@ -1,12 +1,19 @@
 # frozen_string_literal: true
 
 require 'minitest/autorun'
-require_relative '../lib/cool_program'
+require_relative '../lib/monty_hall'
 
 # Test program coolness
 class MontyHallTest < Minitest::Test
-  def test_monty_hall
-    # skip
-    assert_equal CoolProgram.new.coolness, 11
+  def test_initialize_doors
+    assert_equal MontyHall::Game.new().doors, 3
+    assert_equal MontyHall::Game.new(doors: 5).doors, 5
+  end
+
+  def test_initialize_strategy
+    assert_equal MontyHall::Game.new().selected_strategy, :always_switch
+    refute_equal MontyHall::Game.new(selected_strategy: :stay_put).selected_strategy, :always_switch
+    assert_equal MontyHall::Game.new(selected_strategy: :stay_put).selected_strategy, :stay_put
+    assert_equal MontyHall::Game.new(selected_strategy: MontyHall::Game::STRATEGY[1]).selected_strategy, :stay_put
   end
 end
